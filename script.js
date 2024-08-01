bdbf2a5f89f023d3de82d3e60577c5161f8fd75a
SamuelVitor02 <127554098+SamuelVitor02@users.noreply.github.com>
2024-08-01 01:08:00 +0000

    31/07

--- script.js
+++ script.js
@@ -77,7 +77,7 @@ let perguntaAtual;
 
 function mostraAlternativas() {
   for(const alternativa of perguntaAtual.alternativas) {
-    const botaoAlternativas = document.createElement ("button");
+    const botaoAlternativas = document.createElement("button");
     botaoAlternativas.textContent = alternativa.texto;
     botaoAlternativas.addEventListener("click", function(){
       atual++;
